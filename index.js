1cfdbf1fda4415f54ab14f127eedce70fe84a2af
mdgulamasib <gulam.asib@gmail.com>
2022-04-30 15:06:18 +0600

    single service data loaded successfully

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -18,12 +18,24 @@ async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db('mcData').collection('services');
-        app.get('/services', async (req, res) => {
+
+        //services api 
+        app.get('/items', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
             const service = await cursor.toArray();
             res.send(service);
         })
+
+        //finding single service data
+        app.get('/item/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await serviceCollection.findOne(query);
+            res.send(result);
+        });
+
+
     }
     finally {
         //not going to write anything here coz I am going to communicate simultaneously
